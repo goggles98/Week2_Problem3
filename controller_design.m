df42ce814f8be8911c115f9de3c432b186f6cb70
Viraj Gupta <viraj.pro10@gmail.com>
2020-12-11 03:01:38 +0530

    Kp and Ki values found for satisfactory response

--- controller_design.m
+++ controller_design.m
@@ -1,6 +1,20 @@
 s = tf('s');
+
+% Declare the variables
 m = 1000;
 b = 50;
+
+%% For uncompensated system
+% Gs = 1/(m*s+b);
+% sys = feedback(Gs, 1);
+% step(500*sys);
+
+%% Find the values of Kp and Ki:
 Gs = 1/(m*s+b);
-sys = feedback(Gs, 1);
-step(500*sys)
+
+cont = 400*(s+0.1)/s;
+sys = feedback(Gs*cont, 1);
+step(sys);
+
+%Kp = 400 ; Ki = 40
+
